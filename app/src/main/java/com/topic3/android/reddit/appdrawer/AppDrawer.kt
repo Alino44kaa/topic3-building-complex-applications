5173d38a77978a88b08560e8cd39a56a8e38d624
Alino44kaa <greenfox4488@gmail.com>
2024-05-18 05:41:48 +0300

    Реализация тела панели приложения drawer

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -189,6 +189,18 @@ private fun ProfileInfoItem(
 @Composable
 private fun AppDrawerBody(closeDrawerAction: () -> Unit) {
   //TODO add your code here
+  Column {
+    ScreenNavigationButton(
+      icon = Icons.Filled.AccountBox,
+      label = stringResource(R.string.my_profile),
+      onClickAction = { closeDrawerAction() }
+    )
+    ScreenNavigationButton(
+      icon = Icons.Filled.Home,
+      label = stringResource(R.string.saved),
+      onClickAction = { closeDrawerAction() }
+    )
+  }
 }
 
 /**
